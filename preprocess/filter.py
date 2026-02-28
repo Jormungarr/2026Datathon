7216c5a9972e6285fb40e2af140353874c5023b7
Ming593 <ming.lai@mail.utoronto.ca>
2026-02-28 12:07:07 -0500

    initial filter script

---
+++ preprocess/filter.py
@@ -0,0 +1,6 @@
+import pandas as pd
+#load data/airline_ticket_dataset.xlsx
+df = pd.read_excel("data/airline_ticket_dataset.xlsx")
+#print columns names and basic statistics like number of rows
+print("Columns:", df.columns.tolist())
+print("Shape:", df.shape)
