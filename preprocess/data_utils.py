1615d0cf3e29892fff3daa7409a568bb69e643f5
Ming593 <ming.lai@mail.utoronto.ca>
2026-02-28 13:17:55 -0500

    fix data import

--- preprocess/data_utils.py
+++ preprocess/data_utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 def import_orginal_dataset():
     df = pd.read_excel("./data/airline_ticket_dataset.xlsx")
-    df['fare'] = df['fare'].str.replace('$', '', regex=False)
-    df['fare_lg'] = df['fare_lg'].str.replace('$', '', regex=False)
+    df['fare'] = df['fare'].astype(str).str.replace('$', '', regex=False)
+    df['fare_lg'] = df['fare_lg'].astype(str).str.replace('$', '', regex=False)
     return df
 
 COLUMN_UNIT_MAP = {
